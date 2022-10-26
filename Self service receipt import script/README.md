fd145adeb924e0a8cde86c2503c7b3214d37d81c
Muhammad-Sharif Moustafa <mshm@protonmail.com>
2022-10-26 16:35:07 -0400

    Moved the self-service import script into this repo

---
+++ Self service receipt import script/README.md
@@ -0,0 +1,22 @@
+# A Node script that parses a CSV file of fetch tokens and imports them into [RevenueCat](https://www.revenuecat.com) using the REST API.
+**License: MIT**
+
+If you have at least 100k in monthly revenue, you can [reach out](https://www.revenuecat.com/demo/) to perform an assisted bulk receipt import.
+
+Not sure if this self-service script is right for you? [Get in touch](https://app.revenuecat.com/settings/support)!
+
+## ⚠️ Warning!
+This script is automated and performs changes to the data in RevenueCat that may be difficult to reverse. If you're not a developer, have one help you. If you are a developer, test the script on a small subset of the CSV file and check how it looks in RevenueCat before running it on the full file.
+
+## Usage:
+  1. **IMPORTANT: Read the guide to learn more about creating CSV files that can be processed by this script: https://github.com/RevenueCat-Samples/import-csv-samples#readme**
+  2. Download the `import.js` file.
+  3. Fill out the constants at the top of the file with the required information.
+  4. Run `npm install csv axios`.
+  5. Run `node ./import.js`.
+
+Contact support@revenuecat.com or leave a comment below if you have any questions or feedback.
+
+## Learn More
+  1. [RevenueCat API Reference](https://docs.revenuecat.com/reference/basic)
+  2. [Migrating to RevenueCat Using Bulk Imports](https://github.com/RevenueCat-Samples/import-csv-samples#readme)
---
+++ Self service receipt import script/import.js
@@ -0,0 +1,82 @@
+// A Node script that parses a CSV file of fetch tokens and imports them into RevenueCat using the REST API.
+// License: MIT
+
+// Usage:
+//   (1) IMPORTANT: Read the guide to learn more about creating CSV files that can be processed by this script: https://github.com/RevenueCat-Samples/import-csv-samples#readme
+//   (2) Download this file.
+//   (3) Fill out the constants below with the required information.
+//   (4) Run `npm install csv axios`.
+//   (5) Run `node ./import.js`.
+// Contact support@revenuecat.com or leave a comment below if you have any questions or feedback.
+
+// Constants - fill these out before running
+const csvFilePath = 'apple_receipts.csv'; // The relative path to your CSV file
+const apiKey = 'PUBLIC_API_KEY';          // Your public RevenueCat API key. More info: https://docs.revenuecat.com/docs/authentication#obtaining-api-keys
+const platform = 'ios';                   // Can be `ios`, `android`, or `stripe`. Contact support@revenuecat.com to import Mac or Amazon tokens.
+///////////////////////////////////////////////////////////////////////////////
+
+const csvParser = require('csv-parse');
+const fs = require('fs');
+const axios = require('axios').default;
+
+function sleep(ms) {
+  // A convenient function to sleep for a certain amount of time.
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+
+  const parser = fs
+    .createReadStream(csvFilePath)
+    .pipe(csvParser.parse({
+      skipEmptyLines: true,
+      trim: true,
+      columns: true,
+    }));
+
+  let lineNumber = 0;
+  for await (const record of parser) {
+
+    lineNumber++;
+
+    let data = {
+      'app_user_id': record.app_user_id,
+      'fetch_token': record.token,
+      'product_id': record.product_id, // Required on Android and iOS only
+      'price': record.price, // Required on iOS only
+      'currency': record.currency, // Required on iOS only
+      'introductory_price': record.introductory_price, // Required on iOS only if not a free trial
+      'intro_duration': record.introductory_price_duration // Required on iOS only if not a free trial
+    }
+
+    const axiosConfig = {
+      method: 'POST',
+      url: 'https://api.revenuecat.com/v1/receipts',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Authorization': `Bearer ${apiKey}`,
+        'X-Platform': `${platform}`,
+      },
+      data: JSON.stringify(data)
+    };
+
+    try {
+      console.log(`Running request for line ${lineNumber}: ${JSON.stringify(axiosConfig, null, '  ')}`);
+      const response = await axios(axiosConfig);
+    } catch (error) {
+      const status = error.response.status;
+      const data = error.response.data;
+      console.error(`Error importing line ${lineNumber} with status ${status}: ${JSON.stringify(data)}`);
+    }
+
+    await sleep(1000);
+  }
+
+  console.log('\nImport complete!');
+
+}
+
+(async () => {
+  await main();
+})();
